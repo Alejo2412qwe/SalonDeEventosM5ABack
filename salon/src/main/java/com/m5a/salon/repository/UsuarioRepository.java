6e268db06f075bf4ecfe629541317a8e8d4dd371
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2023-06-22 21:57:45 -0500

    login

--- salon/src/main/java/com/m5a/salon/repository/UsuarioRepository.java
+++ salon/src/main/java/com/m5a/salon/repository/UsuarioRepository.java
@@ -14,7 +14,6 @@ import org.springframework.data.repository.query.Param;
  * @author LaptopSA
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
 //    @Query(value = "SELECT rol_id, usu_id, usu_per_id, usu_fecha_registro, usu_contraseña, usu_nombre_usuario FROM usuario WHERE usu_nombre_usuario = :usuario;", nativeQuery = true)
     @Query(value = "SELECT * FROM usuario WHERE usu_nombre_usuario = :usuario;", nativeQuery = true)
     Usuario buscarNombre(@Param("usuario") String usuario);
