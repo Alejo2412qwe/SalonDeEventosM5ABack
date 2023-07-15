c78f895ff53a645d11c1f73696c000dce742e27b
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2023-07-15 18:03:56 -0500

    a

--- salon/src/main/java/com/m5a/salon/genericService/FileService.java
+++ salon/src/main/java/com/m5a/salon/genericService/FileService.java
@@ -34,7 +34,7 @@ public interface FileService {
     /*
     Metodo para borrar todos los archivos cada vez que se inicie el servidor
      */
-    public void deleteAll();
+//    public void deleteAll();
 
     /*
     Metodo para Cargar todos los archivos
